5615e839343d067fafbb8ae2b9fc8a1c2fb3576c
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2022-09-19 14:21:29 +0200

    Create HP_Stage_2.py

---
+++ HP_Stage_2.py
@@ -0,0 +1,34 @@
+# Project: Password Hacker - hyperskills
+# STAGE 2
+import socket
+import sys
+import itertools
+
+data = sys.argv
+hostname = data[1]
+port = int(data[2])
+
+my_socket = socket.socket()
+my_socket.connect((hostname, port))
+
+rep = 1
+signs =  'abcdefghijklmnopqrstuvwxyz0123456789'
+loop_on = True
+while loop_on:
+    passwords = itertools.product(signs, repeat=rep)
+    for p in passwords:
+        p = ''.join(list(p))
+        p = p.encode()
+        my_socket.send(p)
+        response = my_socket.recv(1024)
+        response = response.decode()
+        if response == 'Connection success!':
+            print(p.decode())
+            loop_on = False
+            break
+        if response == 'Too many attempts':
+            break
+
+    rep += 1
+
+my_socket.close()
